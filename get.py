28e7b57cbee619f729b67e438baa1f39ffdfd423
noobdy <htommyf12@gmail.com>
2021-10-28 16:04:31 +0800

    massive refactor

--- get.py
+++ get.py
@@ -1,36 +1,12 @@
 #!/usr/bin/env python
-import json
-import sys
-from typing import Dict
-
-import requests
+from requests import Session
 from bs4 import BeautifulSoup
-import urllib3
-from urllib3.exceptions import InsecureRequestWarning
 
-urllib3.disable_warnings(category=InsecureRequestWarning)
-
-try:
-    with open("login.json", "r") as json_file:
-        login_data: Dict[str, str] = json.load(json_file)
-
-    with open("config.txt", "r") as config:
-        BASE_URL = f"https://140.124.181.{config.readline()}/upload/"
-
-except FileNotFoundError:
-    print("Not yet setup yet")
-    exit(1)
-
-if len(sys.argv) == 1:
-    print("No question selected")
-    exit(1)
+from utils import login
 
 
-with requests.Session() as s:
-    index = f"{int(sys.argv[1]):03}"
-
-    s.post(f"{BASE_URL}/Login", login_data, verify=False)
-    r = s.get(f"{BASE_URL}/showHomework?hwId={index}", verify=False)
+def get(s: Session, url: str, index: str) -> None:
+    r = s.get(f"{url}/showHomework?hwId={index}", verify=False)
 
     soup = BeautifulSoup(r.content.decode("utf-8"), "html5lib")
     res = soup.find(
@@ -38,5 +14,11 @@ with requests.Session() as s:
 
     print(index)
     if res:
-        for line in str(res).split("<br/>")[1:]:
+        for line in str(res).split("<br/>")[1:-1]:
             print(line.strip())
+
+
+if __name__ == "__main__":
+    with Session() as s:
+        url, index = login(s)
+        get(s, url, index)
---
+++ utils.py
@@ -0,0 +1,35 @@
+import json
+import sys
+from typing import Dict, Tuple
+
+from requests import Session
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+
+urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+# Implement decorator here to reduce repetition
+def login(s: Session) -> Tuple[str, str]:
+    try:
+        with open("login.json", "r") as json_file:
+            login_data: Dict[str, str] = json.load(json_file)
+
+        with open("config.txt", "r") as config:
+            base_url = f"https://140.124.181.{config.readline()}/upload/"
+
+    except FileNotFoundError:
+        print("Not yet setup yet")
+        exit(1)
+
+    args = sys.argv
+
+    if len(args) == 1:
+        print("No question selected")
+        exit(1)
+
+    index = f"{int(args[1]):03}"
+
+    s.post(f"{base_url}/Login", login_data, verify=False)
+
+    return base_url, index
