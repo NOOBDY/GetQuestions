bcb4c70c30bb580b03e6aa23f82e96ffba0a8809
noobdy <htommyf12@gmail.com>
2021-10-29 19:01:31 +0800

    added filename generation but haven't finished post request

--- submit.py
+++ submit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-from requests import Session
-from bs4 import BeautifulSoup
+import json
+import sys
+from pprint import pprint
+from time import time
+
 import urllib3
+from bs4 import BeautifulSoup
+from requests import Session
 from urllib3.exceptions import InsecureRequestWarning
 
 from utils import login
@@ -9,11 +14,32 @@ from utils import login
 urllib3.disable_warnings(category=InsecureRequestWarning)
 
 
+def checkStatus(s: Session, url: str, index: str):
+    s.get(f"{url}/upLoadHw?hwId={index}&l=Python")
+
+
 def delete(s: Session, url: str, index: str):
     s.get(f"{url}/delHw?title={index}")
+
+
+def submit(s: Session, url: str, index: str, filepath: str):
+    try:
+        file = open(filepath, "r")
+    except FileNotFoundError:
+        print("Can't find specified file")
+        exit(1)
+
+    filename = f"{json.load(open('login.json'))['name']}_{index}_{round(time() * 1000)}.py"
+
+    r = s.post(f"{url}/upLoadFile", files={"file": file},
+               data={"name": "hwFile", "filename": filename})
+
+    pprint(r.request.body)
+
+    pprint(r.json())
 
 
 if __name__ == "__main__":
     with Session() as s:
         url, index = login(s)
-        delete(s, url, index)
+        submit(s, url, index, sys.argv[2])
