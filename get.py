6f2c3ae89824abd5604fc9e11fd4bcca41eef814
Tommy Huang <htommyf12@gmail.com>
2021-10-30 15:40:02 +0800

    fixed get.py bug where the last line won't be printed out

--- get.py
+++ get.py
@@ -3,7 +3,6 @@ from requests import Session
 from bs4 import BeautifulSoup
 
 from utils import login
-import html
 
 
 def get(s: Session, base_url: str, index: str) -> None:
@@ -13,10 +12,9 @@ def get(s: Session, base_url: str, index: str) -> None:
     res = soup.find(
         "span", style="font-family:標楷體; color:black; behavior:slide; word-wrap:break-word; word-break:normal; font-weight:bold; font-size:medium;")
 
-    print(index)
     if res:
-        for line in str(res).split("<br/>")[1:-1]:
-            print(html.unescape(line.strip()))
+        for line in res.text.strip().split("\n"):
+            print(line.strip())
 
 
 if __name__ == "__main__":
