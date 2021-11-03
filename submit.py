6860b259e4d5cc7b73523a52ac67e5cbf0b6a263
Tommy Huang <htommyf12@gmail.com>
2021-11-03 12:36:37 +0800

    gets status from given index(unfinished)

--- submit.py
+++ submit.py
@@ -4,20 +4,15 @@ import sys
 from time import time
 
 import urllib3
-from bs4 import BeautifulSoup
 from requests import Session
 from urllib3.exceptions import InsecureRequestWarning
 
-from utils import login
+from utils import checkStatus, login
 
 urllib3.disable_warnings(category=InsecureRequestWarning)
 
 
 # i will repurpose this for checking if the test passed
-def checkStatus(s: Session, base_url: str, index: str) -> str:
-    ...
-
-
 def delete(s: Session, url: str, index: str):
     s.get(f"{url}/delHw?title={index}")
 
@@ -35,13 +30,16 @@ def submit(s: Session, base_url: str, index: str, filepath: str):
     url = f"{base_url}/upLoadHw?hwId={index}&l=Python"
     # after digging around i discovered i need to have a GET request first? WTF???
     # maybe it stores a cookie or some shit
-    s.get(url)
+    get_response = s.get(url).content.decode()
 
-    r = s.post(f"{base_url}/upLoadFile",
-               files={"hwFile": (filename, file, "text/x-python")})
+    post_response = s.post(f"{base_url}/upLoadFile",
+                           files={"hwFile": (filename, file, "text/x-python")}).json()
 
-    if r.json()["success"]:
+    if post_response["success"]:
         print("Upload success")
+        # do passing check here
+    else:
+        print(post_response["errorMSg"])
 
 
 if __name__ == "__main__":
@@ -49,5 +47,7 @@ if __name__ == "__main__":
         base_url, index = login(s)
         # i discovered it doesn't matter if you call delete on a un-submitted question
         # i figured calling delete every time would probably be faster since the backend is so slow lmao
-        delete(s, base_url, index)
-        submit(s, base_url, index, sys.argv[2])
+        # delete(s, base_url, index)
+        # submit(s, base_url, index, sys.argv[2])
+
+        checkStatus(s, base_url, index)
--- utils.py
+++ utils.py
@@ -2,8 +2,10 @@ import json
 import sys
 from typing import Dict, Tuple
 
-from requests import Session
 import urllib3
+from bs4 import BeautifulSoup
+from bs4.element import ResultSet, Tag
+from requests import Session
 from urllib3.exceptions import InsecureRequestWarning
 
 urllib3.disable_warnings(category=InsecureRequestWarning)
@@ -31,3 +33,24 @@ def login(s: Session) -> Tuple[str, str]:
     s.post(f"{base_url}/Login", login_data, verify=False)
 
     return base_url, index
+
+
+def checkStatus(s: Session, base_url: str, index: str):
+    r = s.get(f"{base_url}/HomeworkBoard")
+
+    soup = BeautifulSoup(r.content.decode(), "html5lib")
+    res: Tag = soup.find_all("tr")[int(index)]
+
+    if res:
+        elements = [i.get_text().strip() for i in res.find_all("td")]
+
+        print(elements)
+
+        testStatus = {
+            "未繳": -1,
+            "通過": 1,
+            "未通過": 0,
+        }
+
+    isOpen = 0  # closed | now open | not yet open
+    hasPassed = 0  # n/a | failed | passed
